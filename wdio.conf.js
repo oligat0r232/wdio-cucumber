ef6ee5bb145b43951de8cb71b72f4df4c040d1e3
oligat0r232 <olikellynest@gmail.com>
2020-11-04 16:24:09 +0000

    updated

--- wdio.conf.js
+++ wdio.conf.js
@@ -130,13 +130,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: [
-        ['cucumberjs-json', {
-            jsonFolder: '.tmp/new/',
-            language: 'en',
-        },
-        ],
-    ],
+    reporters: ['spec'],
     //
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
